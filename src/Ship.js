1639ae0368661ec82edad4166a25342401a6f2c6
Kyle Bostelmann <kyle@bostelk.ca>
2013-10-19 09:44:28 -0300

    removed unused code.

--- src/Ship.js
+++ src/Ship.js
@@ -25,45 +25,12 @@ Ship.prototype.getSpeed = function() { return 100; }
 Ship.prototype.getX = function() { return this._x; }
 Ship.prototype.getY = function() { return this._y; }
 
-Ship.prototype.act = function() {
-    window.addEventListener("keydown", this);
-};
-
-Ship.prototype.handleEvent = function(e) {
-    var code = e.keyCode;
-    if (code == 13 || code == 32) {
-        this._checkBox();
-        return;
-    }
-
-    var keyMap = {};
-    keyMap[38] = 0;
-    keyMap[33] = 1;
-    keyMap[39] = 2;
-    keyMap[34] = 3;
-    keyMap[40] = 4;
-    keyMap[35] = 5;
-    keyMap[37] = 6;
-    keyMap[36] = 7;
-
-    /* one of numpad directions? */
-    if (!(code in keyMap)) { return; }
-
-    /* is there a free space? */
-    var dir = ROT.DIRS[8][keyMap[code]];
-    var newX = this._x + dir[0];
-    var newY = this._y + dir[1];
-
-    this.move (newX, newY);
-};
-
 var SailRate = {
     FULL : 0,
     HALF : 1,
 };
 
 Ship.prototype.sail = function (rate) {
-
     var eff = this.getSailEff ();
     this.move (this._x + 1, this._y);
 };
