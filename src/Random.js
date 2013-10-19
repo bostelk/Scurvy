e2c1be72059b960dffc5706df9fee5497052a51c
Kyle Bostelmann <kyle@bostelk.ca>
2013-10-19 02:48:55 -0300

    added some random helper functions.

---
+++ src/Random.js
@@ -0,0 +1,19 @@
+var Random = function () {
+
+};
+
+Random.sign = function () {
+    return Math.pow(-1, Random.betweeni(0, 1));
+};
+
+Random.betweenf = function (min, max) {
+    return Math.random() * (max - min) + min;
+};
+
+Random.betweeni = function (min, max) {
+    return Math.floor(Math.random() * (max - min + 1)) + min;
+};
+
+Random.fiftyfifty = function () {
+    return Random.next() >= 0.5;
+};
