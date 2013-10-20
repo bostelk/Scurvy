e7ffd6c11b43641410d7a13ed00176939961fc3e
Kyle Bostelmann <kyle@bostelk.ca>
2013-10-20 10:17:24 -0300

    quick pirate name gen.

--- src/entities/PirateShip.js
+++ src/entities/PirateShip.js
@@ -1,4 +1,5 @@
 var PirateShip = function(x, y) {
+    this.name = PirateShip.generateName ();
     this.x = x;
     this.y = y;
     this.maxHealth = 100;
@@ -12,10 +13,43 @@ PirateShip.prototype.getDamage = function () {
 
 PirateShip.prototype.loseHealth = function (amount) {
     this.health = Math.clamp (this.health - amount, 0, this.maxHealth);
-    if (this.health <= 0)
+    if (this.health <= 0) {
         this.removeNextUpdate = true;
+        G.log ("%c{purple}{0}%c{} sinks to the bottom of the ocean.".format (this.name));
+    }
 };
 
 PirateShip.prototype.draw = function () {
     G.display.draw(this.x, this.y, "P", "red");
+};
+
+PirateShip.generateName = function () {
+    var names = [
+        "Buccaneer's Servant",
+        "Dragon's Madness",
+        "Killer's Damned Secret",
+        "Night's Doom",
+        "Sea's Serpent",
+        "The Greedy Eel",
+        "The Mad Knave",
+        "The Vicious Demon",
+        "The Angry Knave",
+        "The Bloody Slave",
+        "The Coral Doom of Hell",
+        "The Coral Insanity",
+        "The Cursed Sword",
+        "The Dark Grail",
+        "The Death of the South",
+        "The Hate of the Pirate",
+        "The Hell-born Howl",
+        "The Horrible Raider of the South",
+        "The Horrid Barnacle of the North",
+        "The Horror of the North",
+        "The Sad Servant",
+        "The Screaming Knave",
+        "The Shameful Grail",
+        "The Shameful Scream",
+        "The Vile Dagger",
+    ];
+    return names.random ();
 };
